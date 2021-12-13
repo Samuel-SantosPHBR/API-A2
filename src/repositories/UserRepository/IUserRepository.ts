44a54b6d6f9fb9ed2b56a4708bd64c9c1cfb0ccf
Samuel <samueldossantosti@gmail.com>
2021-12-13 20:10:41 -0300

    Versão Inicial do repositorio

--- src/repositories/UserRepository/IUserRepository.ts
+++ src/repositories/UserRepository/IUserRepository.ts
@@ -1,6 +1,12 @@
 import { User } from "../../entities/User";
 
+export interface IUser{
+    name: string;
+    email: string;
+    password: string;
+}
+
 export interface IUserRepository {
-    save(user: User): void;
+    save(user: IUser): void;
     findByEmail(email: string): Promise<User>;
 }
